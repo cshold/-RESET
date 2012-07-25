7042d542109557becaec5b3647011110e637f5d5
Carson Shold <carson@carsonshold.com>
2012-07-25 15:34:07 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+
+	<title></title>
+	
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta property="og:title" content=""/>
+	<meta property="og:type" content="website"/>
+	<meta property="og:url" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="fb:admins" content="100002194886179"/> <!-- Carson Taxi -->
+	<meta property="og:description" content=""/>
+
+	<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/queries.css">
+	
+</head>
+
+<body class="debug">
+
+	<div class="wrapper">
+		<header>
+			Header
+		</header>
+		
+		<div id="main">
+			Main
+		</div>
+		
+		<footer>
+			Footer <a href="#">link</a>
+		</footer>
+	</div> <!--! end of #wrapper -->
+
+	<!-- CSS browser selector and media query pollyfills -->
+	<script src="js/lib/pollyfills.min.js"></script><!-- css browser selector and media query pollyfills -->
+	
+	<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if necessary -->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="js/lib/jquery-1.7.1.min.js"><\/script>')</script>
+	<script src="js/site.js"></script>
+	
+	<script>
+	$(document).ready(function(){
+		reset.Site.init();
+	});
+	</script>
+
+	<script>
+	    var _gaq=[["_setAccount","UA-XXXXX-X"],["_trackPageview"]];
+	    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
+	    g.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";
+	    s.parentNode.insertBefore(g,s)}(document,"script"));
+	  </script>
+
+	<!--[if lt IE 7 ]>
+		<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
+		<script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
+	<![endif]-->
+	
+</body>
+</html>
---
+++ js/site.js
@@ -0,0 +1,51 @@
+window.reset = window.reset || {};
+
+(function (_) {
+	
+	if (_.Site) return;
+	
+	/***************************************************************************
+	Site Class
+	***************************************************************************/
+	
+	_.Site = new function () {
+		return {
+			html: $('html'),
+			body: $('body'),
+			init: function () {
+				
+				this.setMobile();
+			},
+			setMobile: function () {
+				if ( $('html').hasClass('mobile') ) {
+					
+					// scroll below address bar on iphone
+					setTimeout(function(){
+						window.scrollTo(0, 1);
+					}, 0);
+					
+					// viewport bug fix for ipad/iphone
+					var addEvent = 'addEventListener',
+					    type = 'gesturestart',
+					    qsa = 'querySelectorAll',
+					    scales = [1, 1],
+					    meta = qsa in document ? document[qsa]('meta[name=viewport]') : [];
+
+					function fix() {
+						meta.content = 'width=device-width,minimum-scale=' + scales[0] + ',maximum-scale=' + scales[1];
+						document.removeEventListener(type, fix, true);
+					}
+
+					if ((meta = meta[meta.length - 1]) && addEvent in document) {
+						fix();
+						scales = [.25, 1.6];
+						document[addEvent](type, fix, true);
+					}
+				}
+			}
+		}
+	}
+	
+	/******************************************************************************/
+
+})(reset)
